d25686a329a932b6cdcb0e4697ebd897325b6fd3
Akim-13 <akim13.asket@gmail.com>
2012-05-01 09:53:55 +0400

    Added Guard class and few Exception-descendant classes

---
+++ FRS/FRS.Core/Exceptions.h
@@ -0,0 +1,44 @@
+#include <QtCore>
+
+namespace Framework {
+    class BaseException : public QtConcurrent::Exception {
+    public:
+        virtual ~BaseException() {
+            if(_message != NULL) {
+                delete _message;
+                _message = NULL;
+            }
+        }
+
+        void raise() const {
+            throw *this;
+        }
+        virtual Exception *clone() const {
+            return new BaseException(*_message);
+        }
+        virtual QString message() const {
+            return *_message;
+        }
+
+    protected:
+        BaseException() {
+            initialize("");
+        }
+        BaseException(QString const& message) {
+            initialize(message);
+        }
+
+    private:
+        QString* _message;
+
+        void initialize(QString const& message) {
+            _message = new QString(message);
+        }
+    };
+
+    class ArgumentException : public BaseException {
+    public:
+        ArgumentException() : BaseException() {}
+        ArgumentException(QString const& message) : BaseException(message) {}
+    };
+}
---
+++ FRS/FRS.Core/Guard.h
@@ -0,0 +1,28 @@
+#include <QtCore>
+#include "Exceptions.h"
+
+using namespace Framework;
+
+namespace Utils {
+    class Guard {
+    public:
+        template<typename T>
+        static void argumentNotNull(typename T const& argumentValue, QString const& argumentName) {
+            if(argumentValue == NULL)
+                throwException(QString("Argument '%1' can not be null").arg(argumentName));
+        }
+
+        static void argumentNotNullOrEmpty(QString const& argumentValue, QString const& argumentName) {
+            if(argumentValue == NULL || argumentValue.isEmpty())
+                throwException(QString("Argument '%1' can not be null or empty").arg(argumentName));
+        }
+
+    private:
+        Guard() {}
+
+        static void throwException(QString const& message) {
+            ArgumentException* exception = new ArgumentException(message);
+            exception->raise();
+        }
+    };
+}
